1288abf059af96ba6067c303a9810fecbdca7088
avaspataru <spataruava@gmail.com>
2020-10-19 15:42:33 +0100

    Updates

---
+++ precache-manifest.8e17c8762083f57b5b7d7d553e728799.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2492d6c58034579faba555da40e760d9",
+    "url": "/react-web/index.html"
+  },
+  {
+    "revision": "fd185e4eefe361c70bab",
+    "url": "/react-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a916f7cf019488a0bda1",
+    "url": "/react-web/static/js/2.4a6105e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-web/static/js/2.4a6105e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd185e4eefe361c70bab",
+    "url": "/react-web/static/js/main.e2b0cb30.chunk.js"
+  },
+  {
+    "revision": "21816396935d3015ffe2",
+    "url": "/react-web/static/js/runtime-main.ee204452.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-web/static/media/logo.5d5d9eef.svg"
+  }
+]);

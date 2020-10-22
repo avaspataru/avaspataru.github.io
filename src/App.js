31b9872e1410e792434b6bce34e7e35072208f70
avaspataru <ava.spataru@yahoo.com>
2020-10-22 13:50:16 +0100

    hovers and arrows

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ import github from './github-icon.png';
 import linkedin from './linkedin-icon.png';
 import facebook from './facebook-icon.png';
 import email from './email-icon.png';
+import down from './dropdown-icon.png';
 
 
 function App() {
   return (
     <div className="section">
+
+
         <div className="welcome-text">
           <h1> Hi. I'm Ava! </h1>
           <h3 class="line-1 anim-typewriter">Software Engineer & Computational Biologist.</h3>
+
+          <img src={down} className="down-icon anim-fadein" />
+
+
         </div>
 
         <div className="social">
@@ -21,7 +28,6 @@ function App() {
             <a href="https://github.com/avaspataru"><img src={github} className="social-icon" /></a>
             <a href="https://www.facebook.com/ava.spataru/"><img src={facebook} className="social-icon" /></a>
             <a href="mailto:spataruava@gmail.com"><img src={email} className="social-icon" /></a>
-
         </div>
     </div>
 

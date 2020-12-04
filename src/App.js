0670d3cb442a1fa27c0100a41ca94022c1a488b4
avaspataru <ava.spataru@yahoo.com>
2020-12-04 14:53:45 +0000

    attempt at responsive design

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import facebook from './facebook-icon.png';
 import email from './email-icon.png';
 import down from './dropdown-icon.png';
 import profile from './profile-glasses.jpeg';
-import beans from './coffe_beans.png';
+import beans from './coffe_beans_png.png';
 
 import glance from './glance.jpg';
 import battleships from './battleships.jpg';
@@ -83,81 +83,87 @@ function App() {
       </div>
 
       <div className="section-3">
-        <div className="full-width-container">
-          <div className="where-been-text">
-            <h2> <a className="colourful-strong"> <a className="size2"> About where I've been...</a> </a></h2>
+        <div className="content-column">
+          <div className="content-container">
+          <div className="full-width-container">
+            <div className="where-been-text">
+              <h2> <a className="colourful-strong"> <a className="size2"> About where I've been...</a> </a></h2>
 
-              <ul className="where-been-list">
-                <li>
-                 <div className={(selectedWork == "fb") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("fb")}> Facebook </div>
-                </li>
-                <li>
-                  <div className={(selectedWork == "tw") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("tw")}> Transferwise </div>
-                </li>
-                <li>
-                  <div className={(selectedWork == "ms") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("ms")}> Morgan Stanley</div>
-                </li>
-                <li>
-                  <div className={(selectedWork == "gs") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("gs")}> Gemini Solutions </div>
-                </li>
-              </ul>
-              <div className="work-details">
-                  {(selectedWork == "fb") ?
-                  <div>
-                    <p className="job-header">Graduate Software Engineer <b className="company-highlight">@Facebook</b></p>
-                    <p className="job-timeline">Dec 2020 - Present</p>
-                    <ul className="job-points">
-                    <p className="job-description"><li> I haven't actually started the job just yet, but I am very very excited! </li></p>
-                    <p className="job-description"><li> I will update this as soon as I have what to write. </li></p>
-                    </ul>
-                  </div>
-
-                  : <div></div>}
-
-                  {(selectedWork == "tw") ?
-                      <div>
-                      <p className="job-header">Full-Stack Bootcamp Engineer <b className="company-highlight">@TransferWise</b></p>
-                      <p className="job-timeline">Jun 2019 - Sept 2019</p>
+                <ul className="where-been-list">
+                  <li>
+                   <div className={(selectedWork == "fb") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("fb")}> Facebook </div>
+                  </li>
+                  <li>
+                    <div className={(selectedWork == "tw") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("tw")}> Transferwise </div>
+                  </li>
+                  <li>
+                    <div className={(selectedWork == "ms") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("ms")}> Morgan Stanley</div>
+                  </li>
+                  <li>
+                    <div className={(selectedWork == "gs") ? "where-been-box-selected" : "where-been-box"} onClick={() => setSelectedWork("gs")}> Gemini Solutions </div>
+                  </li>
+                </ul>
+                <div className="work-details">
+                    {(selectedWork == "fb") ?
+                    <div>
+                      <p className="job-header">Graduate Software Engineer <b className="company-highlight">@Facebook</b></p>
+                      <p className="job-timeline">Dec 2020 - Present</p>
                       <ul className="job-points">
-                      <p className="job-description"><li>My work was in the Education team to develop a web applicatication as a solution for international tuition fee payments. </li> </p>
-                      <p className="job-description"><li>The solution was a payment platform and a payment managing system for students, parents, sponsor companies and universities with one or multiple international campuses.</li></p>
+                      <p className="job-description"><li> I haven't actually started the job just yet, but I am very very excited! </li></p>
+                      <p className="job-description"><li> I will update this as soon as I have what to write. </li></p>
                       </ul>
-                      <a href="https://www.youtube.com/watch?v=cEvPTOgJx70&t=1s">See a video of my bootcamp experience!</a>
-                      </div>
-                      : <div></div>}
+                    </div>
 
-                  {(selectedWork == "ms") ?
+                    : <div></div>}
+
+                    {(selectedWork == "tw") ?
                         <div>
-                        <p className="job-header">Summer Analyst in Technology <b className="company-highlight">@Morgan Stanley</b></p>
-                        <p className="job-timeline">Jun 2018 - Aug 2018</p>
+                        <p className="job-header">Full-Stack Bootcamp Engineer <b className="company-highlight">@TransferWise</b></p>
+                        <p className="job-timeline">Jun 2019 - Sept 2019</p>
                         <ul className="job-points">
-                        <p className="job-description"><li>I worked in the Listed Sales and Trading team to solve the issue of legacy code debugging, by building a helper tool.</li></p>
-                        <p className="job-description"><li>The tool was using reflection techniques to look into the codebase and assist the debugging, while also providing insightful analytics.</li></p>
+                        <p className="job-description"><li>My work was in the Education team to develop a web applicatication as a solution for international tuition fee payments. </li> </p>
+                        <p className="job-description"><li>The solution was a payment platform and a payment managing system for students, parents, sponsor companies and universities with one or multiple international campuses.</li></p>
                         </ul>
+                        <a href="https://www.youtube.com/watch?v=cEvPTOgJx70&t=1s">See a video of my bootcamp experience!</a>
                         </div>
                         : <div></div>}
 
-                  {(selectedWork == "gs") ?
-                        <div>
-                        <p className="job-header">Programmer Help <b className="company-highlight">@Gemini Solutions</b></p>
-                        <p className="job-timeline">Aug 2017 - Sept 2017</p>
-                        <ul className="job-points">
-                        <p className="job-description"><li>I worked on an internal monitoring application for keeping track of employee responsibility and project statuses across the company.</li></p>
-                        <p className="job-description"><li>The project involved rewriting an reshaping an existing tool into a React-based website.</li></p>
-                        </ul>
-                        </div>
-                        : <div></div>}
+                    {(selectedWork == "ms") ?
+                          <div>
+                          <p className="job-header">Summer Analyst in Technology <b className="company-highlight">@Morgan Stanley</b></p>
+                          <p className="job-timeline">Jun 2018 - Aug 2018</p>
+                          <ul className="job-points">
+                          <p className="job-description"><li>I worked in the Listed Sales and Trading team to solve the issue of legacy code debugging, by building a helper tool.</li></p>
+                          <p className="job-description"><li>The tool was using reflection techniques to look into the codebase and assist the debugging, while also providing insightful analytics.</li></p>
+                          </ul>
+                          </div>
+                          : <div></div>}
 
-              </div>
+                    {(selectedWork == "gs") ?
+                          <div>
+                          <p className="job-header">Programmer Help <b className="company-highlight">@Gemini Solutions</b></p>
+                          <p className="job-timeline">Aug 2017 - Sept 2017</p>
+                          <ul className="job-points">
+                          <p className="job-description"><li>I worked on an internal monitoring application for keeping track of employee responsibility and project statuses across the company.</li></p>
+                          <p className="job-description"><li>The project involved rewriting an reshaping an existing tool into a React-based website.</li></p>
+                          </ul>
+                          </div>
+                          : <div></div>}
+
+                </div>
+            </div>
+
           </div>
-        </div>
 
-        <div className="full-width-container">
-          <div className="what-learnt-text">
-          <h2> <a className="colourful-strong"> <a className="size2"> ... and what I learnt</a> </a></h2>
+          <div className="full-width-container">
+            <div className="what-learnt-text">
+            <h2> <a className="colourful-strong"> <a className="size2"> ... and what I learnt</a> </a></h2>
 
-          <img src={beans} className="language-beans"/>
+            <img src={beans} className="language-beans"/>
 
+            </div>
+
+          </div>
           </div>
         </div>
       </div>
